a4760ee7b9967da8a21bb2ba9883a40b41149eb2
iamonuwacj <onuwachinedu66@gmail.com>
2024-09-18 11:01:09 +0100

    Commenced the todoList project and installed tailwind and daisyui for the todolist project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+    return (
+        <div>
+            <button class="btn btn-primary">One</button>
+            <button class="btn btn-secondary">Two</button>
+            <button class="btn btn-accent btn-outline">Three</button>
+        </div>
+    )
+}
+
+export default App
